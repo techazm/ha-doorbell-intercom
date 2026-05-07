d2b04133187eb19a3cdf62129ddef8a1c53ae445
techazm <services@azmtech.com.au>
2026-05-07 21:42:33 +1000

    fix: simplify run.sh - remove bashio shebang to work with s6-overlay

--- doorbell_intercom/run.sh
+++ doorbell_intercom/run.sh
@@ -1,13 +1,7 @@
-#!/usr/bin/with-contenv bashio
-# shellcheck shell=bash
+#!/bin/bash
+set -e
 
-bashio::log.info "Starting Doorbell Intercom add-on..."
-bashio::log.info "Reading configuration..."
-
-# Export the full add-on config as JSON for the Node server
-export ADDON_CONFIG
-ADDON_CONFIG="$(bashio::config '.')"
-
-bashio::log.info "Configured doorbells: $(bashio::config 'doorbells | length')"
+echo "Starting Doorbell Intercom add-on v1.0.1..."
+echo "ADDON_CONFIG: $ADDON_CONFIG"
 
 exec node /app/server.js
